20f90bc91805eda8300152257dbc4db602f3c1b1
THKHA <THKHA@ASUS>
2020-07-11 00:13:06 +0700

    Bottom sheet_Check user join group

--- app/src/main/java/com/example/appchat/Adapter/UserAddGroupAdapter.java
+++ app/src/main/java/com/example/appchat/Adapter/UserAddGroupAdapter.java
@@ -4,8 +4,9 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,7 +15,10 @@ import com.bumptech.glide.Glide;
 import com.example.appchat.Model.User;
 import com.example.appchat.R;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserAddGroupAdapter  extends RecyclerView.Adapter<UserAddGroupAdapter.ViewHolder>{
 
@@ -25,6 +29,11 @@ public class UserAddGroupAdapter  extends RecyclerView.Adapter<UserAddGroupAdapt
     public UserAddGroupAdapter(Context mContext, List<User> mUser) {
         this.mContext = mContext;
         this.mUser = mUser;
+        this.arrUserJoinGroup = new ArrayList<>();
+    }
+
+    public List<String> getArrUserJoinGroup() {
+        return arrUserJoinGroup;
     }
 
     @NonNull
@@ -37,13 +46,29 @@ public class UserAddGroupAdapter  extends RecyclerView.Adapter<UserAddGroupAdapt
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
             User u = mUser.get(position);
 
             holder.tvUserName.setText(u.getUsername());
-            if(u.getImageURL().equals("default")){
-                Glide.with(mContext).load(u.getImageURL()).into(holder.ivAvatar);
+            if(! u.getImageURL().equals("default")){
+               Glide.with(mContext).load(u.getImageURL()).into(holder.ivAvatar);
             }
+
+            holder.cbCheckUser.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(holder.cbCheckUser.isChecked()){
+                       // Toast.makeText(mContext, mUser.get(position).getUsername(), Toast.LENGTH_SHORT).show();
+                        arrUserJoinGroup.add(mUser.get(position).getId());
+                    }else{
+                        if(arrUserJoinGroup.contains( mUser.get(position).getId()) ){
+                            int p = arrUserJoinGroup.indexOf(mUser.get(position).getId());
+                            Toast.makeText(mContext, p+"", Toast.LENGTH_SHORT).show();
+                            arrUserJoinGroup.remove(p);
+                        }
+                    }
+                }
+            });
     }
 
     @Override
@@ -53,13 +78,15 @@ public class UserAddGroupAdapter  extends RecyclerView.Adapter<UserAddGroupAdapt
 
     public class ViewHolder  extends RecyclerView.ViewHolder{
 
-        public ImageView ivAvatar;
+        public CircleImageView ivAvatar;
         public TextView tvUserName;
+        public CheckBox cbCheckUser;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            ivAvatar = itemView.findViewById(R.id.CircleImageView_Avatar_Group);
+            ivAvatar = itemView.findViewById(R.id.CircleImageView_avatar_user);
             tvUserName = itemView.findViewById(R.id.TextView_Name_User);
+            cbCheckUser = itemView.findViewById(R.id.checkbox_add_user_group);
         }
     }
 }
--- app/src/main/java/com/example/appchat/CreateGroupBottomSheetDialog.java
+++ app/src/main/java/com/example/appchat/CreateGroupBottomSheetDialog.java
@@ -5,16 +5,40 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.appchat.Adapter.UserAddGroupAdapter;
+import com.example.appchat.Model.User;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateGroupBottomSheetDialog  extends BottomSheetDialogFragment {
     private BottomSheetBehavior mBehavior;
+    private RecyclerView rvListUser;
+    private FirebaseAuth fAuth;
+    private FirebaseUser fUser;
+    private FirebaseDatabase fDatabase;
+    private DatabaseReference dRef;
+
+    private UserAddGroupAdapter userAddGroupAdapter;
+    private List<User> arrListUser;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,23 +55,64 @@ public class CreateGroupBottomSheetDialog  extends BottomSheetDialogFragment {
         LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) linearLayout.getLayoutParams();
         params.height =getFullScreen();
         linearLayout.setLayoutParams(params);
-
         bottomSheetDialog.setContentView(view);
+
         mBehavior = BottomSheetBehavior.from( (View) view.getParent());
         mBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+
+
+        arrListUser = new ArrayList<>();
+
+        rvListUser = view.findViewById(R.id.RecyclerView_List_User_Add_Group);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL, false);
+        rvListUser.setLayoutManager(layoutManager);
+
+
+        LoadUserJoinGroup();
 
         view.findViewById(R.id.ImageButton_Create_Group).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                if(userAddGroupAdapter.getArrUserJoinGroup().size() > 0){
+                    Toast.makeText(getContext(), userAddGroupAdapter.getArrUserJoinGroup().get(0), Toast.LENGTH_SHORT).show();
+                }
                 mBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
             }
         });
-
 
         return bottomSheetDialog;
     }
 
     public static int getFullScreen(){
         return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
+
+    private void LoadUserJoinGroup(){
+        fAuth = FirebaseAuth.getInstance();
+        fUser = fAuth.getCurrentUser();
+        fDatabase = FirebaseDatabase.getInstance();
+
+        dRef = fDatabase.getReference("Users");
+        dRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User u = snapshot.getValue(User.class);
+                    if(! u.getId().equals(fUser.getUid())){
+                        arrListUser.add(u);
+                    }
+                }
+               // userAddGroupAdapter.notifyDataSetChanged();
+                userAddGroupAdapter = new UserAddGroupAdapter(getContext(), arrListUser);
+                rvListUser.setAdapter(userAddGroupAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
